8c203c3e700c25b77e3a370fd4a8f860585fcd2d
Juan Ferraro <juansebastianferraro@gmail.com>
2023-07-07 09:40:11 -0500

    First views configuration! 📚

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Main Page! Platzi Awards")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"This is the Question #{question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Here are the results of Question #{question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting for Question #{question_id}")
